2e3e9c410dafd30a0c505f814400e9b54bc7eb5c
MichaelNkong <nkongho.ndip@yahoo.com>
2020-09-07 09:27:36 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,40 @@
+
+
+var tipcalculator={
+bills:[124,48,268,180,42,8000,9000,400,24],
+tip:[],
+final:[],
+ 
+cal:function(){
+    ;
+    
+   for(var i=0;i<=this.bills.length; i++){
+
+   if(this.bills[i]<50){
+   this.tip[i]=(this.bills[i])*0.2;
+   this.final[i]=this.bills[i] + this.tip[i];
+   
+   }
+   else if(50 <= this.bills[i] && this.bills[i]<=200){
+    this.tip[i]=(this.bills[i])*0.5;
+    this.final[i]=this.bills[i] + this.tip[i];
+    }
+   else if(this.bills[i]>200){
+    this.tip[i]=(this.bills[i])*0.1;
+    this.final[i]=this.bills[i] + this.tip[i];
+    }
+
+    }
+    
+  }
+
+}
+
+
+var urAnswer ={
+var answer=['yes','no'];
+var 
+
+}
+
+answers
